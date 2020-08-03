4f5f755d5507339a2ccfdd28cf024d9ada6917fa
ChaosCrafter908 <68608362+ChaosCrafter908@users.noreply.github.com>
2020-08-03 15:58:25 +0200

    Update Main.php

--- src/ChaosCrafter908/Main.php
+++ src/ChaosCrafter908/Main.php
@@ -1,1 +1,37 @@
+<?php
+
+Namespace: ChaosCrafter908;
+
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+
+class main extends PluginBase implements Listener {
+
+public function onEnable(){
+    $this->getLogger()->info(“test”);
+}
+public function onDisable(){
+    $this->getLogger()->info(“test”);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
